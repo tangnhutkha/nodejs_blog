1991f9a2d5e998e191d5e06ada8d7c4b96c58ec5
Kha Tang <tangnhutkha209@gmail.com>
2023-02-06 11:29:26 +0700

    add file public/css folder to gitignore

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+    extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('PATH: ', path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port localhost:${port}`)
+})  
